fe88ae5369d5dce897a93c9da97a31c01a0589b4
ofalei <45478711+ofalei@users.noreply.github.com>
2020-09-21 22:20:12 +0700

    Added an iface package to the library (#2)
    * added an iface package
    
    * added a setUp method to a Client structure
    
    * Add a new method to the mocking interface; remove a setup method

---
+++ zoomiface/interface.go
@@ -0,0 +1,13 @@
+package zoomiface
+
+import "github.com/teamexos/zoom-lib-golang"
+
+// ClientAPI provides an interface for Zoom Client
+type ClientAPI interface {
+	CreateMeeting(opts zoom.CreateMeetingOptions) (zoom.Meeting, error)
+	ListMeetings(opts zoom.ListMeetingsOptions) (zoom.ListMeetingsResponse, error)
+	RegisterForMeeting(opts zoom.MeetingRegistrant) (zoom.RegisterForMeetingResponse, error)
+}
+
+// make sure zoom.Client type satisfies the ClientAPI interface
+var _ ClientAPI = (*zoom.Client)(nil)
